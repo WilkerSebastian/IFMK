1d93aebebf40882cdc514e730a9449e5c855b939
WilkerAfonso <wilkerafonso2005@gmail.com>
2022-04-24 00:34:33 -0400

    base

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+const nav = document.getElementById('navegacao')
+
+const footer = document.getElementById("footer")
+
+nav.style.height = window.outerHeight
+
+footer.style.width = window.outerWidth
